0c159d8db8a70a24230f82353770be8e27673920
HenryFanDi <abcdz330@gmail.com>
2022-03-24 11:58:32 +0800

    Add: ESLint

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello! I'm Henry Fhd!</p>
+        <p>Hello! I&#39;m Henry Fhd!</p>
         <p>
-          (This is a sample website - you'll be building a site like this on{' '})
+          (This is a sample website - you&#39;ll be building a site like this on{' '})
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
         </p>
       </section>
